0ae32288bd95bf42e339915859348e3d7208b27c
Edward Chan <chankaward@gmail.com>
2014-01-13 00:14:23 +0800

    update vpn.zsh

--- oh-my-zsh/custom/vpn.zsh
+++ oh-my-zsh/custom/vpn.zsh
@@ -1,15 +1,15 @@
 function vpn {
 /usr/bin/env osascript <<-EOF
 tell application "System Events"
-	tell current location of network preferences
-		set VPNservice to service "VPNCloud" -- name of the VPN service
-		set isConnected to connected of current configuration of VPNservice
-		if isConnected then
-			disconnect VPNservice
-		else
-			connect VPNservice
-		end if
-	end tell
+  tell current location of network preferences
+    set VPNservice to service "VPNCloud (L2TP)" -- name of the VPN service
+    set isConnected to connected of current configuration of VPNservice
+        if isConnected then
+          disconnect VPNservice
+        else
+          connect VPNservice
+        end if
+  end tell
 end tell
 EOF
 }
